3f8ab68efe73f526e20d4104f8b3e9fbb0f2ceca
MuhammadaliAliyev <muhammadalialiyev4174@gmail.com>
2026-04-21 23:33:58 +0900

    change admin.py

--- handlers/errors/error_handler.py
+++ handlers/errors/error_handler.py
@@ -1,58 +1,33 @@
 import logging
-from aiogram.utils.exceptions import (Unauthorized, InvalidQueryID, TelegramAPIError,
-                                      CantDemoteChatCreator, MessageNotModified, MessageToDeleteNotFound,
-                                      MessageTextIsEmpty, RetryAfter,
-                                      CantParseEntities, MessageCantBeDeleted)
-
+from aiogram.exceptions import (
+    TelegramUnauthorizedError, TelegramAPIError, TelegramRetryAfter,
+    TelegramBadRequest
+)
+from aiogram.types import ErrorEvent
 
 from loader import dp
 
-
-@dp.errors_handler()
-async def errors_handler(update, exception):
+@dp.error()
+async def errors_handler(event: ErrorEvent):
     """
-    Exceptions handler. Catches all exceptions within task factory tasks.
-    :param dispatcher:
-    :param update:
-    :param exception:
-    :return: stdout logging
+    Exceptions handler. Catches all exceptions.
     """
+    exception = event.exception
 
-    if isinstance(exception, CantDemoteChatCreator):
-        logging.exception("Can't demote chat creator")
-        return True
-
-    if isinstance(exception, MessageNotModified):
-        logging.exception('Message is not modified')
-        return True
-    if isinstance(exception, MessageCantBeDeleted):
-        logging.exception('Message cant be deleted')
-        return True
-
-    if isinstance(exception, MessageToDeleteNotFound):
-        logging.exception('Message to delete not found')
-        return True
-
-    if isinstance(exception, MessageTextIsEmpty):
-        logging.exception('MessageTextIsEmpty')
-        return True
-
-    if isinstance(exception, Unauthorized):
+    if isinstance(exception, TelegramUnauthorizedError):
         logging.exception(f'Unauthorized: {exception}')
         return True
 
-    if isinstance(exception, InvalidQueryID):
-        logging.exception(f'InvalidQueryID: {exception} \nUpdate: {update}')
+    if isinstance(exception, TelegramRetryAfter):
+        logging.exception(f'RetryAfter: {exception} \nUpdate: {event.update}')
+        return True
+
+    if isinstance(exception, TelegramBadRequest):
+        logging.exception(f'BadRequest: {exception} \nUpdate: {event.update}')
         return True
 
     if isinstance(exception, TelegramAPIError):
-        logging.exception(f'TelegramAPIError: {exception} \nUpdate: {update}')
-        return True
-    if isinstance(exception, RetryAfter):
-        logging.exception(f'RetryAfter: {exception} \nUpdate: {update}')
-        return True
-    if isinstance(exception, CantParseEntities):
-        logging.exception(f'CantParseEntities: {exception} \nUpdate: {update}')
+        logging.exception(f'TelegramAPIError: {exception} \nUpdate: {event.update}')
         return True
         
-    logging.exception(f'Update: {update} \n{exception}')
+    logging.exception(f'Update: {event.update} \n{exception}')
--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -5,12 +5,22 @@ from aiogram import F
 from data.config import ADMINS
 from loader import dp, db, bot
 
-@dp.message(F.text == "/allusers", F.from_user.id.in_(list(map(int, ADMINS))))
+# Sanitize admin IDs
+def get_admin_ids():
+    admin_ids = []
+    for a in ADMINS:
+        try:
+            admin_ids.append(int(str(a).strip('[]"\' ')))
+        except ValueError:
+            pass
+    return admin_ids
+
+@dp.message(F.text == "/allusers", F.from_user.id.in_(get_admin_ids()))
 async def get_all_users(message: types.Message):
     users = await db.select_all_users()
     await message.answer(str(users))
 
-@dp.message(F.text == "/reklama", F.from_user.id.in_(list(map(int, ADMINS))))
+@dp.message(F.text == "/reklama", F.from_user.id.in_(get_admin_ids()))
 async def send_ad_to_all(message: types.Message):
     users = await db.select_all_users()
     for user in users:
@@ -21,7 +31,7 @@ async def send_ad_to_all(message: types.Message):
             pass
         await asyncio.sleep(0.05)
 
-@dp.message(F.text == "/cleandb", F.from_user.id.in_(list(map(int, ADMINS))))
+@dp.message(F.text == "/cleandb", F.from_user.id.in_(get_admin_ids()))
 async def clean_all_users(message: types.Message):
     await db.delete_users()
     await message.answer("Baza tozalandi!")
